696892b811ce52cf61372b1e1287ccf1ba769554
CamdenPix <camdengenta@gmail.com>
2023-11-14 13:31:31 -0600

    Update some information

--- Lab7/Controllers/HomeController.cs
+++ Lab7/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Lab7.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Made by Luke, Kene, Sam, and Camden.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us:";
 
             return View();
         }
